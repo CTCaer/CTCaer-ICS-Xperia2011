b26d53888569691ba60fe2f07836de13e4579ff3
CTCaer <ctcaer@gmail.com>
2013-02-22 07:13:09 +0200

    tweaked and fixed Governors

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -402,6 +402,9 @@ extern struct cpufreq_governor cpufreq_gov_lazy;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ONDEMANDX)
 extern struct cpufreq_governor cpufreq_gov_ondemandx;
 #define CPUFREQ_DEFAULT_GOVERNOR  (&cpufreq_gov_ondemandx)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTASS2)
+extern struct cpufreq_governor cpufreq_gov_smartass2;
+#define CPUFREQ_DEFAULT_GOVERNOR  (&cpufreq_gov_smartass2)
 #endif
 
 
